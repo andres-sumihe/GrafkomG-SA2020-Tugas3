60b3115c50e4a261cdad8b22f79b5e014bf44b9d
andres-sumihe <672018136@student.uksw.edu>
2020-06-24 18:41:10 +0700

    add glutMouseFunc and simplification

--- GrafkomG-SA2020-Tugas3/GrafkomG-SA2020-Tugas3.cpp
+++ GrafkomG-SA2020-Tugas3/GrafkomG-SA2020-Tugas3.cpp
@@ -1,47 +1,46 @@
 #include <iostream>
+//#include <GL/glut.h>
 #include <GL/freeglut.h>
 #include <vector>
 using namespace std;
 int MouseX = 0, MouseY = 0;
-
-vector < int > arr;
-vector<int>::iterator cek;
-
+vector<char> arr;
+vector<char>::iterator cek;
 void printOut(void);
-
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glFlush();
-	
 }
 void keyboardDown(unsigned char key, int x, int y) {
 	cek = find(arr.begin(), arr.end(), key);
-	if (cek == arr.end())
-		arr.push_back(key);
+	if (cek == arr.end()) arr.push_back(key);
 	printOut();
 }
 
 void keyboardUp(unsigned char key, int x, int y) {
-	
-	 cek = find(arr.begin(), arr.end(), key);
-	
+	cek = find(arr.begin(), arr.end(), key);
 	if (cek != arr.end()) {
 		int index = distance(arr.begin(), cek);
 		arr.erase(arr.begin() + index);
-	}
-	printOut();
-	
+	} printOut();
 }
 void printOut() {
 	system("cls");
 	cout << "MouseX: " << MouseX << " | " << "MouseY: " << MouseY << endl;
-	for (auto it : arr)
-		cout << char(it) << ' ';
+	for(auto it : arr) cout << it << ' ';
 }
 
 void mouse(int x, int y) {
 	MouseX = x;
 	MouseY = y;
+	printOut();
+}
+
+void mouseButton(int button, int state, int x, int y) {
+	if (button == GLUT_LEFT_BUTTON && (state == GLUT_DOWN || state == GLUT_UP)) {
+		MouseX = x;
+		MouseY = y;
+	}
 	printOut();
 }
 void myinit() {
@@ -58,14 +57,11 @@ int main(int argc, char** argv) {
 	glutInitWindowPosition(0, 0);
 	glutCreateWindow("Empty");
 	glutDisplayFunc(display);
-	glutMotionFunc(mouse);
-
-
+	glutMotionFunc(mouse);				  // when mouse move
+	glutMouseFunc(mouseButton);			  // when left mouse button clicked
 	glutKeyboardFunc(keyboardDown);       // when a key is down
 	glutKeyboardUpFunc(keyboardUp);       // when the key goes up
-
 	myinit();
 	glutMainLoop();
-
 	return 0;
 }
